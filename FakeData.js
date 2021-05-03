53e4abf681b7189c3b95355bfdd4e5ea376acdb8
Rayzon3 <rahul.03111999@gmail.com>
2021-05-03 13:41:41 +0530

    basic setup for apollo graphQL

---
+++ FakeData.js
@@ -0,0 +1,30 @@
+users = [
+    {
+        name: "Asuna",
+        age: 20,
+        fav_anime: "Sword Art Online" 
+    },
+    {
+        name: "Chika",
+        age: 19,
+        fav_anime: "Hyouka" 
+    },
+    {
+        name: "Asuka",
+        age: 23,
+        fav_anime: "Evangelion" 
+    },
+    {
+        name: "Mizuhara",
+        age: 20,
+        fav_anime: "Rent a girlfriend" 
+    },
+    {
+        name: "Kirito",
+        age: 19,
+        fav_anime: "Sword Art Online" 
+    }
+
+]
+
+module.exports = {users}
---
+++ index.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require("apollo-server")
+const gql = require('graphql-tag')
+const { Server } = require("ws")
+
+const { users } = require("./FakeData")
+
+const typeDefs = gql`
+    type User {
+        name: String!
+        age: Int!
+        fav_anime: String!
+    }
+    type Query {
+        getUsers: [User!]!
+    }
+`
+
+const resolvers = {
+    Query: {
+        getUsers(){
+            try {
+                return users
+            }catch(err){
+                throw new Error(err)
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen({port: 5000})
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
